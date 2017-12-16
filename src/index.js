6228f852278ae8db871bd1d8367f25479f31ebad
Wes Basinger <wbasinger@villagetechschools.org>
2017-12-16 13:58:45 +0000

    Drawing rectangles onto the screen.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("working");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const drawRectangle = (x, y, width, height, color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+drawRectangle(10, 10, 100, 100, "green");
+
+drawRectangle(200, 200, 100, 100, "red");
